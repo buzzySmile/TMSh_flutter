ab4b6c4aa6d9a1763a692b055033ed7e36454a7a
buzzysmile <makarov.san@gmail.com>
2019-06-28 17:15:41 +0300

    Initial commit; stub sources

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tmsh_flutter/ui/search_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: SearchScreen(title: 'TMSh Demo'),
+    );
+  }
+}
